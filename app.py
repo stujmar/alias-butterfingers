8102033902367f81bfd07adc292ee523158c19e9
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-02-06 22:50:31 -0600

    'commit'

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import os
+import sys
 
-git_add_all = "git add ."
-os.system(git_add_all)
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+
+if "add" in sys.argv:
+  print("Adding")
+  git_add_all = "git add ."
+  os.system(git_add_all)
+
+if "commit" in sys.argv:
+  print("Commiting")
+  git_commit = "git commit -m '" + sys.argv[2] + "'"
+  os.system(git_commit)
+
