8277ab54fb4be9a89d448394d785c7aaa40a342e
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-02-06 22:55:50 -0600

    'commandline'

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ print('Argument List:', str(sys.argv))
 
 if "add" in sys.argv:
   print("Adding")
-  git_add_all = "git add ."
+  git_add_all = "git add --all"
   os.system(git_add_all)
 
-if "commit" in sys.argv:
+if "message" in sys.argv:
   print("Commiting")
-  git_commit = "git commit -m '" + sys.argv[2] + "'"
+  git_commit = "git commit -m '" + sys.argv[3] + "'"
   os.system(git_commit)
+
+if "push" in sys.argv:
+  print("Pushing")
+  git_push = "git push"
+  os.system(git_push)
+
 
